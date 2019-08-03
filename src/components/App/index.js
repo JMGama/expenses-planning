371265478a4f20140761a18cb9894ac653917982
Jose M. Gama Estrada <josegamaes@gmail.com>
2019-08-03 13:01:37 -0500

    Delete styles from App component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,22 +1,10 @@
 import React from "react";
-import { Title, Paragraph } from "./styles";
 import { Navbar } from "../Navbar";
+import { Card } from "../Card";
 
 export const App = () => (
   <div>
     <Navbar />
-    <Title>
-      <span className="mx-1" role="img" aria-label="money-with-wings">
-        💸
-      </span>
-      Expenses Planning
-      <span role="img" aria-label="money-with-wings">
-        💸
-      </span>
-    </Title>
-    <Paragraph>
-      Welcome to what will be the future application to manage your money and
-      plan your expenses the right way.
-    </Paragraph>
+    <Card />
   </div>
 );
--- src/components/App/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Title = styled.h1`
-  text-align: center;
-`;
-
-export const Paragraph = styled.p`
-  text-align: center;
-`;
