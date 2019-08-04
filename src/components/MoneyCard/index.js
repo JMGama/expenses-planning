9f2b7faba87e646ef3f9b841d81dfc084b43fc97
Jose M. Gama Estrada <josegamaes@gmail.com>
2019-08-04 11:02:12 -0500

    Add MoneyCard component

---
+++ src/components/MoneyCard/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { MdAttachMoney } from "react-icons/md";
+
+export const MoneyCard = props => (
+  <div className="text-center">
+    <div className="font-weight-light">
+      <span>{props.text}</span>
+    </div>
+
+    <div className={"shadow d-inline-flex rounded my-1 border " + props.border}>
+      <div className="align-middle p-2">
+        <MdAttachMoney size="1.2em" className="align-middle" />
+        <span className="align-middle mr-1">{props.value}</span>
+      </div>
+    </div>
+  </div>
+);
