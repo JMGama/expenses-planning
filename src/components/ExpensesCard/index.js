c9246d2217119ba7c7b08d62958e8b23c97ac749
Jose M. Gama Estrada <josegamaes@gmail.com>
2019-08-05 22:11:47 -0500

    Update Expenses Card to react-bootstrap

--- src/components/ExpensesCard/index.js
+++ src/components/ExpensesCard/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { ExpensesCardHeader } from "../ExpensesCardHeader";
+import Card from "react-bootstrap/Card";
 
 export const ExpensesCard = props => (
-  <div className="card border-0 shadow mx-3 mx-sm-3">
+  <Card className="shadow mx-3 mx-sm-3">
     <ExpensesCardHeader month={props.month} values={props.values} />
-    <div className="card-body p-3">something</div>
-  </div>
+    <Card.Body className="p-3">something</Card.Body>
+  </Card>
 );
