9935906924315a9929b25758eee5bae5e5b23263
Jose M. Gama Estrada <josegamaes@gmail.com>
2019-08-08 18:16:20 -0500

    Add new income/outcome structure

--- src/components/StatusBar/index.js
+++ src/components/StatusBar/index.js
@@ -1,60 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import { MoneyCard } from "../MoneyCard";
+import { Select } from "./styles";
+
 import { FiPlus } from "react-icons/fi";
+import { FiDollarSign } from "react-icons/fi";
+
+import CurrencyInput from "react-currency-input";
+
 import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
-import Dropdown from "react-bootstrap/Dropdown";
+import Col from "react-bootstrap/Col";
+import InputGroup from "react-bootstrap/InputGroup";
+import Form from "react-bootstrap/Form";
 
-export const StatusBar = () => (
-  <Card className="shadow">
-    <Card.Body className="p-3 container">
-      <Row className="align-items-center">
-        <div className="col-6">
-          <Row>
-            <div className="col">
-              <MoneyCard
-                value="16,000.00"
-                text="Income"
-                border="border-success"
-              />
-            </div>
+export const StatusBar = () => {
+  const [selectedValue, setSelectedValue] = useState("Income");
+  return (
+    <Card className="shadow">
+      <Card.Body className="p-3 container">
+        <Row className="align-items-center">
+          <div className="col-6 mt-2">
+            <Form>
+              <Form.Group as={Row} controlId="AddValue.Select">
+                <Form.Label column>New:</Form.Label>
+                <Col>
+                  <Select as="select" className="form-control shadow">
+                    <option className="text-success" value="Income">
+                      Income
+                    </option>
+                    <option className="text-danger" value="Outcome">
+                      Outcome
+                    </option>
+                  </Select>
+                </Col>
+              </Form.Group>
 
-            <div className="col">
-              <MoneyCard
-                value="5,000.00"
-                text="Outcome"
-                border="border-danger"
-              />
-            </div>
+              <Form.Group as={Row} controlId="AddValue.Select">
+                <Col>
+                  <InputGroup className="shadow">
+                    <InputGroup.Prepend>
+                      <InputGroup.Text>
+                        <FiDollarSign className="align-middle" />
+                      </InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <CurrencyInput className="form-control" />
+                  </InputGroup>
+                </Col>
+              </Form.Group>
+            </Form>
+          </div>
 
-            <div className="col">
-              <MoneyCard
-                value="5,000.00"
-                text="Balance"
-                border="border-primary"
-              />
-            </div>
-          </Row>
-        </div>
-        <div className="col-6 text-right">
-          <Dropdown>
-            <Dropdown.Toggle
-              variant="info"
-              id="dropdown-basic"
-              className="shadow"
-            >
-              <FiPlus className="align-middle" />{" "}
-              <span className="align-middle mr-1">Add</span>
-            </Dropdown.Toggle>
+          <div className="col-6">
+            <Row>
+              <div className="col">
+                <MoneyCard
+                  value="16,000.00"
+                  text="Income"
+                  border="border-success"
+                />
+              </div>
 
-            <Dropdown.Menu>
-              <Dropdown.Item href="#/action-1">Income</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Outcome</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Estimation</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </div>
-      </Row>
-    </Card.Body>
-  </Card>
-);
+              <div className="col">
+                <MoneyCard
+                  value="5,000.00"
+                  text="Outcome"
+                  border="border-danger"
+                />
+              </div>
+
+              <div className="col">
+                <MoneyCard
+                  value="5,000.00"
+                  text="Balance"
+                  border="border-primary"
+                />
+              </div>
+            </Row>
+          </div>
+        </Row>
+      </Card.Body>
+    </Card>
+  );
+};
---
+++ src/components/StatusBar/styles.js
@@ -0,0 +1,8 @@
+import Styled from "styled-components";
+
+export const Select = Styled.select`
+
+    && option.selected === "Income" {
+        background-color: black !important;
+    }
+`;
