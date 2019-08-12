2e0b7f6fc6a835819ffe464e8b4e0a6a612560e9
Jose M. Gama Estrada <josegamaes@gmail.com>
2019-08-11 22:59:50 -0500

    Change date format (not the best)

--- src/components/NewValueCard/index.js
+++ src/components/NewValueCard/index.js
@@ -6,6 +6,7 @@ import { FiPlus } from "react-icons/fi";
 import CurrencyInput from "react-currency-input";
 
 import DayPickerInput from "react-day-picker/DayPickerInput";
+import { DateUtils } from "react-day-picker";
 import "react-day-picker/lib/style.css";
 
 import Card from "react-bootstrap/Card";
@@ -14,10 +15,27 @@ import InputGroup from "react-bootstrap/InputGroup";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
+import dateFnsFormat from "date-fns/format";
+import dateFnsParse from "date-fns/parse";
+
+function parseDate(str, format, locale) {
+  const parsed = dateFnsParse(str, format, { locale });
+  if (DateUtils.isDate(parsed)) {
+    return parsed;
+  }
+  return undefined;
+}
+
+function formatDate(date, format, locale) {
+  return dateFnsFormat(date, format, { locale });
+}
+
 export const NewValueCard = () => {
   const [selectedValue, setSelectedValue] = useState("Income");
   const [amount, setAmount] = useState(0.0);
   const [description, setDescription] = useState("");
+
+  const FORMAT = "MM/DD/YYYY";
 
   return (
     <Card className="shadow">
@@ -77,6 +95,10 @@ export const NewValueCard = () => {
 
             <Col sm="2" xs="6">
               <DayPickerInput
+                formatDate={formatDate}
+                format={FORMAT}
+                parseDate={parseDate}
+                placeholder={`${dateFnsFormat(new Date(), FORMAT)}`}
                 inputProps={{
                   className:
                     "shadow form-control form-control-sm text-center font-weight-normal"
