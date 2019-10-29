117599c7aff9c03a8b3c7346d45030d7ce1f58b8
Jose M. Gama Estrada <josegamaes@gmail.com>
2019-10-28 22:39:32 -0600

    Add total to MonthExpensesCard

--- src/components/MonthExpensesCard/index.js
+++ src/components/MonthExpensesCard/index.js
@@ -2,13 +2,37 @@ import React from "react";
 
 import Card from "react-bootstrap/Card";
 import Table from "react-bootstrap/Table";
+import InputGroup from "react-bootstrap/InputGroup";
+import CurrencyInput from "react-currency-input";
+import { FiDollarSign } from "react-icons/fi";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 
 
 export const MonthExpensesCard = ({ header }) => {
   return (
     <Card className="shadow mx-3 mx-sm-3">
       <Card.Header as="h6" className="py-2">
-        {header}
+        <Row>
+          <Col className="align-self-center">{header}</Col>
+          <Col>
+            <InputGroup size="sm">
+              <InputGroup.Prepend>
+                <InputGroup.Text>
+                  <b>Total</b>
+                  {/* <FiDollarSign className="align-middle" /> */}
+                </InputGroup.Text>
+              </InputGroup.Prepend>
+              <CurrencyInput
+                className="form-control"
+                value="100"
+              />
+            </InputGroup>
+          </Col>
+        </Row>
+
+
       </Card.Header>
       <Card.Body className="p-3">
         <Table responsive striped bordered hover size="sm" className="mb-0">
