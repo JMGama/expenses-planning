e78bb7a165fae09a509915f4469da8c1a66313c4
Jose M. Gama Estrada <josegamaes@gmail.com>
2020-02-25 17:07:56 -0600

    Server logic for expenses and months

--- server/src/routes/months.js
+++ server/src/routes/months.js
@@ -4,6 +4,7 @@ const {
 const fs = require('fs')
 const router = Router();
 
+
 router.get('/', (req, res) => {
     let months = JSON.parse(fs.readFileSync(__dirname + '/../db/months.json', 'utf8'))
 
@@ -14,7 +15,8 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     let months = JSON.parse(fs.readFileSync(__dirname + '/../db/months.json', 'utf8'))
 
-    const response = months.find(item => item.id === req.params.id);
+    const response = months.find(item => item.id === parseInt(req.params.id)
+    );
     res.send(response);
 });
 
@@ -24,23 +26,5 @@ router.get('/:month/:year', (req, res) => {
     const response = months.find(item => item.month === req.params.month && item.year === req.params.year);
     res.send(response);
 });
-
-router.post('/', (req, res) => {
-    let months = JSON.parse(fs.readFileSync(__dirname + '/../example.json', 'utf8'))
-
-    //get the month
-    let date = new Date(req.body.date)
-    let monthToChange = String(date.getMonth() + 1)
-    let yearOfMonth = String(date.getFullYear())
-    let month = months.find(item => item.month === monthToChange && item.year === yearOfMonth)
-    console.log(req.body)
-    res.send('success')
-    //change month total and add the new one.
-    // month[req.body.type].expenses.push(req.body)
-    // month[req.body.type].total += req.body.value
-
-    // ///save data to file
-    // fs.writeFileSync(__dirname + '/../example.json', JSON.stringify(months))
-})
 
 module.exports = router;
