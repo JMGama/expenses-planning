aec9abec57618f3341e0f00d30be5cb8f2547fc9
Jose M. Gama Estrada <josegamaes@gmail.com>
2020-03-31 13:35:58 -0600

    Validate user data to laod Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,37 +1,50 @@
 import React from "react";
 
 import { ExpensesCard } from "../../components/ExpensesCard";
+import { useUser } from '../../context/user-context'
+import { Redirect } from "react-router-dom";
+
+
 
 import Row from "react-bootstrap/Row";
 
-export const Home = () => (
-  <div>
-    <Row className="card-deck my-3">
-      {/* This div can have a max of 3 Expenses cards, if there are more you need to create another div */}
-      <ExpensesCard
-        month="August"
-        values={{
-          income: "10,000.00",
-          outcome: "3,000.00",
-          balance: "7,000.00"
-        }}
-      />
-      <ExpensesCard
-        month="July"
-        values={{
-          income: "10,000.00",
-          outcome: "5,000.00",
-          balance: "5,000.00"
-        }}
-      />
-      <ExpensesCard
-        month="June"
-        values={{
-          income: "10,000.00",
-          outcome: "8,000.00",
-          balance: "2,000.00"
-        }}
-      />
-    </Row>
-  </div>
-);
+export const Home = () => {
+  const userContext = useUser()
+  console.log(userContext.user)
+  if (userContext.user.id == null) {
+    return <Redirect to="/login" />
+  } else {
+    return (
+      <div>
+        <Row className="card-deck my-3">
+          {/* This div can have a max of 3 Expenses cards, if there are more you need to create another div */}
+          <ExpensesCard
+            month="August"
+            values={{
+              income: "10,000.00",
+              outcome: "3,000.00",
+              balance: "7,000.00"
+            }}
+          />
+          <ExpensesCard
+            month="July"
+            values={{
+              income: "10,000.00",
+              outcome: "5,000.00",
+              balance: "5,000.00"
+            }}
+          />
+          <ExpensesCard
+            month="June"
+            values={{
+              income: "10,000.00",
+              outcome: "8,000.00",
+              balance: "2,000.00"
+            }}
+          />
+        </Row>
+      </div>
+    );
+  }
+
+}
